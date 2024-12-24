f765d2d1b8100d091eca56fd6dd7116232942694
zitske <eduardomontzitske@gmail.com>
2024-12-24 17:41:21 -0300

    cleaning

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from utils import calculate_radius
-from collections import namedtuple
-
-point = namedtuple('point', ['longitude', 'latitude', 'altitude', 'velocity'])
 
 
 v = 120; # Aircraft speed in knots
 a = 30; # Curver angle in degrees
-
-
-# Example usage
-aircraft_state = point(longitude=34.0522, latitude=-118.2437, altitude=10000, velocity=120)
 
 radius = calculate_radius(v, a)
 
