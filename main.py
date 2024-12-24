4a65e04f2863c2e21586077aeb2d06455e74fa95
zitske <eduardomontzitske@gmail.com>
2024-12-24 17:33:01 -0300

    units conversions and plotter

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from math import tan, radians
-
+import numpy as np
+import matplotlib.pyplot as plt
 #(Vˆ2)/11.26*tg(30)
 
-v = 120; #km/h
+v = 120; # Aircraft speed in knots
+a = 30; # Curver angle in degrees
+
+def calculate_radius(v,a):
+    #v= v/1.852; #Uncoment to use km/h instead of knots on input
+    angle_in_radians = radians(a)
+    result = (v**2)/(11.26*tan(angle_in_radians));
+    result = result*0.3048; #Convert to meters
+    return result;
 
 
-def calculate(v):
-    v= v/1.852; #knots
-    angle_in_radians = radians(30)
-    return (v**2)/(11.26*tan(angle_in_radians))
+radius = calculate_radius(v, a)
 
-print(calculate(v));
+theta = np.linspace(0, 2 * np.pi, 100)
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
+
+plt.plot(x, y)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title(f'Curve with radius: {radius:.2f} meters')
+plt.xlabel('x (meters)')
+plt.ylabel('y (meters)')
+plt.grid(True)
+plt.show()
